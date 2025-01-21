d5581367cd3896ff3acb8b1fc03320830f889b65
lenka.kocianova <lenka.koc@seznam.cz>
2025-01-21 11:11:17 +0100

    nainstalovany plugin @stylistic/eslint-plugin-js a do eslint config dopsáno import stylistic a napsán stylistic do seznamu pluginu

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import stylistic from '@stylistic/eslint-plugin'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
